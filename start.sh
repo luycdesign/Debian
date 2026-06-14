79df9d59f645cb6223265d859485e94a62994940
luycdesign <luyaochang65+design@gmail.com>
2026-06-14 05:00:07 -0700

    Start Pinggy tunnel in start.sh
    Added SSH command to establish a reverse tunnel to Pinggy.

--- start.sh
+++ start.sh
@@ -7,4 +7,12 @@ service xrdp start
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
+echo "[*] Starting Pinggy tunnel..."
+ssh -p 443 \
+    -o StrictHostKeyChecking=no \
+    -o ServerAliveInterval=30 \
+    -o ExitOnForwardFailure=yes \
+    -R 0:127.0.0.1:3389 \
+    tcp@free.pinggy.io &
+
 tail -f /var/log/xrdp-sesman.log
