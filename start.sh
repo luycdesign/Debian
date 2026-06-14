dfb571c6fb72a0f54abcd12794378ea4cc9c836a
luycdesign <luyaochang65+design@gmail.com>
2026-06-14 05:40:42 -0700

    Refactor start.sh for pulseaudio and logging
    Updated the start script to improve logging and run pulseaudio in the background.

--- start.sh
+++ start.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+echo "root:${RDP_PASSWORD:-changeme}" | chpasswd
+
 service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
+pulseaudio --system --disallow-exit --disable-shm &
 service xrdp start
 
-mkdir -p /tmp/.X11-unix
-chmod 1777 /tmp/.X11-unix
-
+echo "[*] Starting Pinggy tunnel..."
 while true; do
-    echo "[*] Starting Pinggy tunnel..."
     ssh -p 443 \
         -o StrictHostKeyChecking=no \
         -o ServerAliveInterval=30 \
         -o ExitOnForwardFailure=yes \
         -R 0:127.0.0.1:3389 \
         tcp@free.pinggy.io
-    echo "[!] Tunnel dropped, reconnecting in 5s..."
+    echo "[*] Tunnel dropped, reconnecting in 5s..."
     sleep 5
 done &
 
